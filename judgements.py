2abbfa3daf8d734b22bd28f21acb77cc14a44338
Robert Dyer <rdyer@unl.edu>
2022-03-15 14:43:58 -0500

    generate single judgement table

--- judgements.py
+++ judgements.py
@@ -5,19 +5,16 @@ import pandas as pd
 import common
 
 
-# %%
-categories = common.get_categories()
-df = pd.read_csv('data/judgements.csv', header=0, index_col=False)
-
 # %% [markdown]
 # # Generate Table(s)
 
 # %%
-df2 = df.groupby('Human').size().to_frame('test').rename({'test': ''}, axis=1)
-df2 = df2.reindex(categories[1:4])
-common.save_table(df2, 'human-judgements', decimals=0)
+df = pd.read_csv('data/judgements.csv',
+                 header=0,
+                 index_col=False,
+                 usecols=['Human', 'Machine'])
 
-# %%
-df3 = df.groupby('Machine').size().to_frame('test').rename({'test': ''}, axis=1)
-df3 = df3.reindex(categories[1:4])
-common.save_table(df3, 'machine-judgements', decimals=0)
+df2 = pd.DataFrame({'Human Judgements': df.groupby(['Human']).size(),
+                   'Machine Judgements': df.groupby(['Machine']).size()})
+
+common.save_table(df2, 'judgements', decimals=0)
