71bcb35eff9c4b2587a6254beb6d787f3ff90df6
Robert Dyer <rdyer@unl.edu>
2022-03-15 14:12:28 -0500

    list main branch files in dataset stats table

--- stats-table.py
+++ stats-table.py
@@ -34,15 +34,33 @@ counts = df3.describe().apply(lambda s: s.apply('{0:.2f}'.format))
 common.save_table(counts, 'counts-dist', decimals=2, escape=True)
 
 # %%
+try:
+    rq1nodupes = pd.read_parquet('data/parquet/rq1-nodupes.parquet')
+except:
+    try:
+        df = pd.read_parquet('data/parquet/rq1.parquet')
+    except:
+        df = common.get_data('data/csv/rq1.output.csv', ['var', 'project', 'file', 'revid', 'commitdate', 'classification'], ['var', 'revid', 'commitdate'], common.get_counts())
+        df = common.filter_projects(df)
+        df.to_parquet('data/parquet/rq1.parquet', compression='gzip')
+
+    rq1nodupes = common.remove_dupes(df)
+    rq1nodupes = common.split_categories(rq1nodupes, categories)
+
+    rq1nodupes['classified'] = rq1nodupes.apply(common.classify_file, axis=1)
+    rq1nodupes.to_parquet('data/parquet/rq1-nodupes.parquet', compression='gzip')
+
 projs = len(dfnodupes.project.unique())
 revs = len(dfnodupes.commitdate.unique())
 files = len(dfnodupes.groupby(['project', 'file']).first())
+filesmain = len(rq1nodupes.groupby(['project', 'file']).first())
 snapshots = len(dfnodupes)
 asts = dfnodupes.groupby(['project']).first()['ast_count'].sum()
 
-df = pd.DataFrame(data=[projs, revs, files, snapshots, asts], index=['\\textbf{Projects}',
+df = pd.DataFrame(data=[projs, revs, files, filesmain, snapshots, asts], index=['\\textbf{Projects}',
                    '\\textbf{Revisions}',
                    '\\textbf{Python Files}',
+                   '\\rowcolor{gray!15}\\qquad\\textbf{Python Files (main branch)}',
                    '\\textbf{Python File Snapshots}',
                    '\\textbf{ASTs}'])
 
